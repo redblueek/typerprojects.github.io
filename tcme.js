3f8068ea7fde0c412bd69be7c1c6eeb6490a8f96
redblueek <106463491+redblueek@users.noreply.github.com>
2026-04-03 12:40:36 +0200

    fix?

--- tcme.js
+++ tcme.js
@@ -1,4 +1,4 @@
-console.log(navigator.userAgent); console.log(localStorage.getItem("typer/pkgs")); localStorage.setItem("typer/asm", 'say Typer Command Exploit, successfully loaded in ASMM'); let allpackages = localStorage.getItem("typer/pkgs"); if(allpackages.includes("cws")) { execute("tcm", "tcm rm cws"); } localStorage.setItem("typer/preasm", "tcm i cwss")
+console.log(navigator.userAgent); console.log(localStorage.getItem("typer/pkgs")); console.log("pass 1 stage"); localStorage.setItem("typer/asm", "say Typer Command Exploit, successfully loaded in ASMM"); let allpackages = localStorage.getItem("typer/pkgs"); if(allpackages.includes("cws")) { execute("tcm", "tcm rm cws"); } else { localStorage.setItem("typer/preasm", "tcm i cwss") }
 async (data) => {
   return "This command should not be executed in the Command Interface. Abort.";
 }
