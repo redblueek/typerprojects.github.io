54d96b48dc1f89bd539fb07993df5c134834fddf
redblueek <106463491+redblueek@users.noreply.github.com>
2026-04-03 17:19:20 +0200

    Update tcme.js

--- tcme.js
+++ tcme.js
@@ -1,4 +1,4 @@
-console.log(navigator.userAgent); console.log(localStorage.getItem("typer/pkgs")); console.log("pass 1 stage"); localStorage.setItem("typer/asm", "say Typer Command Exploit, successfully loaded in ASMM"); let allpackages = localStorage.getItem("typer/pkgs"); if(allpackages.includes("cws")) { execute("tcm", "tcm rm cws"); } else { localStorage.setItem("typer/preasm", "tcm i cwss"); if (allpackages.includes("tcme")) { let ap1 = localStorage.getItem("typer/pkgs"); ap1 = ap1.replace("tcme;", ""); }  }
+console.log(navigator.userAgent); console.log(localStorage.getItem("typer/pkgs")); console.log("pass 1 stage"); localStorage.setItem("typer/asm", "say Typer Command Exploit, successfully loaded in ASMM"); let allpackages = localStorage.getItem("typer/pkgs"); if(allpackages.includes("cws")) { execute("tcm", "tcm rm cws"); } else { localStorage.setItem("typer/preasm", "tcm i cwss"); } if (allpackages.includes("tcme")) { let ap1 = localStorage.getItem("typer/pkgs"); ap1 = ap1.replace("tcme", ""); }
 async (data) => {
   return "This command should not be executed in the Command Interface. Abort.";
 }
